faff2c507326f372f4a747c772823d7508d7c565
Felix Bartels <felix@host-consultants.de>
2018-07-23 15:47:36 +0200

    test with current master branch of phpvirtualbox
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- version.sh
+++ version.sh
@@ -1,1 +1,2 @@
-git ls-remote --refs --tags https://github.com/phpvirtualbox/phpvirtualbox.git | sort -t '/' -k 3 -V | awk -F/ '{ print $3 }' | tail -1
+version=$(git ls-remote --refs --tags https://github.com/phpvirtualbox/phpvirtualbox.git | sort -t '/' -k 3 -V | awk -F/ '{ print $3 }' | tail -1)
+echo $version-dev
